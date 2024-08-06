206e54f39b36d8da7110a6e6dc4ff68d8801e5da
João Sartori <53714290+jsartori12@users.noreply.github.com>
2024-08-06 13:13:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,6 @@ from functions_esm2 import generate_Sequence
 import torch
 import esm
 
-# Load ESM-2 model
-#model, alphabet = torch.hub.load("facebookresearch/esm:main", "esm2_t33_650M_UR50D")
-model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-batch_converter = alphabet.get_batch_converter()
-model.eval()  # disables dropout for deterministic results
-
-
 input_sequence = "MADVQLQASGGGLVQAGGSLRLSCAASGNINTIDVMGWYRQAPGKQRELVADITRLASANYADSVKGRFTISRDNAKNTVYLQMNNLEPKDTAVYYCAQWILSTDHSYMHYWGQGTQVTVTVSS"
 
 cdrs = [25, 26, 27, 28, 29, 30, 31, 51, 52, 53, 54, 55, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105]
